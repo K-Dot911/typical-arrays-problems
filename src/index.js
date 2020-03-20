3e99e022a7b32edd981328b957d6a9792e346e5c
K-Dot911 <60567755+K-Dot911@users.noreply.github.com>
2020-03-20 21:55:00 +0300

    update code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 exports.min = function min (array) {
     var min=array[0];
+    if(array.length===0)
+    {
+        return 0;
+    }
     for( var i =1; i<array.length; i++)
     {
       if(array[i]<min)
@@ -13,6 +17,10 @@ exports.min = function min (array) {
 
 exports.max = function max (array) {
     var max=array[0];
+    if(array.length===0)
+    {
+        return 0;
+    }
     for( var i =1; i<array.length; i++)
    {
     if(array[i]>max)
@@ -26,9 +34,13 @@ exports.max = function max (array) {
 
 exports.avg = function avg (array) {
     var avg=0;
+    if(array.length===0)
+    {
+        return 0;
+    }
     for( var i =0; i<array.length; i++)
     {
-      avg+=array[i]/array.length;
+      avg+=(array[i]/array.length);
     }
     return avg;
 }
