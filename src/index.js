f3459154f79cf994ad61c683a76639e88a7a5b4f
K-Dot911 <60567755+K-Dot911@users.noreply.github.com>
2020-03-20 21:34:18 +0300

    start task

---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+exports.min = function min (array) {
+    var min=array[0];
+    for( var i =1; i<array.length; i++)
+    {
+      if(array[i]<min)
+      {
+        min=array[i];
+      }
+    }
+    return min;
+}
+
+exports.max = function max (array) {
+    var max=array[0];
+    for( var i =1; i<array.length; i++)
+   {
+    if(array[i]>max)
+    {
+      max=array[i];
+    }
+   }
+   return max;
+}
+
+exports.avg = function avg (array) {
+    var avg=0;
+    for( var i =0; i<array.length; i++)
+    {
+      avg+=array[i]/n;
+    }
+    return avg;
+}
