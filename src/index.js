87046fd0476e968ce18b297cf0ea4d25095e5da8
K-Dot911 <60567755+K-Dot911@users.noreply.github.com>
2020-03-20 22:16:04 +0300

    feat: update code

--- src/index.js
+++ src/index.js
@@ -1,46 +1,15 @@
 
 exports.min = function min (array) {
-    var min=array[0];
-    if(array.length==0 || array==undefined)
-    {
-        return 0;
-    }
-    for( var i =1; i<array.length; i++)
-    {
-      if(array[i]<min)
-      {
-        min=array[i];
-      }
-    }
-    return min;
-}
-
-exports.max = function max (array) {
-    var max=array[array.length-1];
-    if(array.length==0 || array==undefined)
-    {
-        return 0;
-    }
-    for( var i =array.length-2; i>=0; i--)
-   {
-    if(array[i]>max)
-    {
-      max=array[i];
-    }
-   }
-   return max;
-}
-
-
-exports.avg = function avg (array) {
-    var avg=0;
-    if(array.length==0 || array==undefined)
-    {
-        return 0;
-    }
-    for( var i =0; i<array.length; i++)
-    {
-      avg+=(array[i]/array.length);
-    }
-    return avg;
-}
+    if (array == undefined || array.length == 0) return 0;
+    return Math.min(...array);
+  }
+  
+  exports.max = function max (array) {
+    if (array == undefined || array.length == 0) return 0;
+    return Math.max(...array);
+  }
+  
+  exports.avg = function avg (array) {
+    if (array == undefined || array.length == 0) return 0;
+    return array.reduce( (sum, i) => sum + i ) / array.length;
+  }
